1ab80508f8315b0558d1d7be319e34c6b1cff40c
lohith921 <nanuvala.lohith@gmail.com>
2015-10-29 15:56:02 -0500

    Program to write the mesh information into .node .ele files

---
+++ My work/write_file.m
@@ -0,0 +1,12 @@
+function [ ] = write_file( p, e, t, Hess, filebase )
+% write_file writes the mesh information into .node .ele files etc
+%   Detailed explanation goes here
+type1='node';
+type2='ele';
+type3='hess';
+file1 = sprintf('%s.%s',filebase,type1);
+
+
+
+end
+
