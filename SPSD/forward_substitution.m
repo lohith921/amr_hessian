ba1a4040721372f1ff47e0767832b43aa4855377
lohith921 <nanuvala.lohith@gmail.com>
2016-02-16 18:07:26 -0600

    Final version of forward substitution

--- SPSD/forward_substitution.m
+++ SPSD/forward_substitution.m
@@ -2,10 +2,12 @@ function [ y ] = forward_substitution( L,b )
 % This program computes the y in Ly=b using forward substitution method.
 [n,~] = size(L);
 y = zeros(n,1);
-y(1) = b(1)/L(1,1);
-for i=2:n
-    j = i-1;
-    y(i) = (b(i)-L(i,j)*y(j))/L(i,i);
+% y(1) = b(1)/L(1,1);
+% for i=2:n
+%     j = i-1;
+%     y(i) = (b(i)-L(i,j)*y(j))/L(i,i);
+for i=1:n
+ y(i) = (b(i)-L(i,:)*y)/L(i,i);
 end
 end
 
