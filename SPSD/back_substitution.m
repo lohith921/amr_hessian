52fd1832d1bf35999cc9a17f0e7e3cf1da669eed
lohith921 <nanuvala.lohith@gmail.com>
2016-02-16 18:07:43 -0600

    Final version of back substitution

--- SPSD/back_substitution.m
+++ SPSD/back_substitution.m
@@ -4,10 +4,13 @@ function [ x ] = back_substitution( Lt,y )
 % is vector.
 [n,~] = size(Lt);
 x = zeros(n,1);
-x(n) = y(n) / Lt(n,n);
-for i = n-1:-1:1
-    j = i+1;
-    x(i) = (y(i)-L(i,j)*y(i))/L(i,i);
+% x(n) = y(n) / Lt(n,n);
+% for i = n-1:-1:1
+%     j = i+1;
+%     x(i) = (y(i)-Lt(i,j)*y(i))/Lt(i,i);
+% end
+for i = n:-1:1
+   x(i) = ( y(i) - Lt(i, :)*x )/Lt(i, i);
 end
 end
 
