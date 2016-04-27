62c861169f9a23d82686ef83b9b3cedb03b75f82
lohith921 <nanuvala.lohith@gmail.com>
2016-04-27 17:57:08 -0500

    Function which returns the neighbor triangle depending on the shared edge passed

---
+++ find_neighbor_with.m
@@ -0,0 +1,16 @@
+% This function is used to find the neighbor triangle which shares edge
+% with current triangle. p1, p2 are vertices of the edge. tri is the array
+% of triangles. The triangle number is returned in n and the index of third
+% vertex is returned in ot
+function [n, ot] = find_neighbor_with(p1, p2, tri)
+    n = find((tri(:,1) == p1) & (tri(:,2) == p2));
+    ot = 3;
+    if isempty(n)
+        n = find((tri(:,2) == p1) & (tri(:,3) == p2));
+        ot = 2;
+        if isempty(n)
+            n = find((tri(:,3) == p1) & (tri(:,1) == p2));
+            ot = 1;
+        end
+    end
+end
