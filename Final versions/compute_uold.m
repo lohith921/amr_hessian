e39ef0963f20df9dd3e16a1b10a7581e4440143b
lohith921 <nanuvala.lohith@gmail.com>
2016-05-04 22:26:56 -0500

    Made some cosmetic changes

--- Final versions/compute_uold.m
+++ Final versions/compute_uold.m
@@ -10,16 +10,16 @@ function [val] = compute_uold(o_mesh, sol, Xg)
         xmax = max([X1(1) X2(1) X3(1)]);
         ymin = min([X1(2) X2(2) X3(2)]);
         ymax = max([X1(2) X2(2) X3(2)]);
-        check = (Xg(1) < xmin) && (Xg(1) > xmax) && (Xg(2) < ymin) && (Xg(2) > ymax);
+        check = ((Xg(1) < xmin) || (Xg(1) > xmax)) && ((Xg(2) < ymin) || (Xg(2) > ymax));
         if ~check
             bool = checkinside((X1'), (X2'), (X3'), Xg);
             if (bool == 1) % This point lies on or inside current triangle.
-                A = det([(X2' - X1') (X3' - X1')])/2;
-                A1 = det([(Xg - X2') (Xg - X3')])/2;
-                A2 = det([(Xg - X1') (Xg - X3')])/2;
-                A3 = det([(Xg - X1') (Xg - X2')])/2;
+                A = det([(X2' - X1') (X3' - X1')])/2;			% signed area of the whole triangle
+                A1 = det([(Xg - X2') (Xg - X3')])/2;			% signed area of triangle formed by Xg, X2, X3
+                A2 = det([(Xg - X1') (Xg - X3')])/2;			% signed area of triangle formed by Xg, X1, X3
+                A3 = det([(Xg - X1') (Xg - X2')])/2;			% signed area of triangle formed by Xg, X2, X1
                 %phi1 = A1/A;      phi2 = A2/A;      phi3 = A3/A;
-                phi = [A1 A2 A3]/A;
+                phi = [A1 A2 A3]/A;								% row vector of interpolation function values
                 val = phi*sol(tri_nodes');
                 break;
             end
