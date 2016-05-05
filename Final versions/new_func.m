42bec387f5b856816cd8772caa14e157e8e3b3c4
lohith921 <nanuvala.lohith@gmail.com>
2016-05-05 10:07:20 -0500

    code modified to suit the requirements

--- Final versions/new_func.m
+++ Final versions/new_func.m
@@ -1,7 +1,7 @@
 % This function is the right hand side f in the poisson's equation. deltaU = f;
-function output = new_func(input_1, input_2, coeff)
+function output = new_func(input_1, input_2)
 	%output =sin(input_1);
 	%output = coeff;
 	%output = 2*((input_1-2.5)^3+(input_2-2.5)^3);
-	output = coeff;
+	output = 1.0;
 end
